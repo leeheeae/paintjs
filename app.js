452c3eedd2c2b3d96a31945cb5a708bc09a5b91e
leeheeae <chromeee@daum.net>
2022-02-22 15:39:28 +0900

    stroke color change

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('controls__color');
+
+console.log(Array.from(colors)); //배열로 변경
 
 //default setting
 canvas.width = 700;
@@ -29,9 +32,20 @@ function stopPainting() {
   painting = false;
 }
 
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+//캔버스 마우스 핸들러 설정
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
+
+//컬러 핸들러 설정
+Array.from(colors).forEach((color) =>
+  color.addEventListener('click', handleColorClick)
+);
