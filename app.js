c07444351bd6f642a4b5dfb81f059c969fb1318b
leeheeae <chromeee@daum.net>
2022-02-22 15:31:44 +0900

    라인 그리기

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+//default setting
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokeStyle = '#2c2c2c'; //선 색
+ctx.lineWitdh = 2.5; //선 사이즈
 
 let painting = false;
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+function startPainting() {
+  painting = true;
 }
 
 function stopPainting() {
   painting = false;
 }
 
-function onMouseDown() {
-  painting = true;
-}
-
-function onMouseUp() {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
